e28b1b90606c3564a5e80f0368573c21ef1b27f2
nrv-ous <38772631+nrv-ous@users.noreply.github.com>
2019-09-05 19:07:58 -0700

    Create timer.h

---
+++ timer.h
@@ -0,0 +1,36 @@
+namespace std
+{
+  template<typename unit>
+  class timer
+  {
+    unit interval;
+    bool ticking;
+  public:
+    timer(unit interval) : interval(interval)
+    {
+    }
+
+    void start()
+    {
+      ticking = true;
+    }
+
+    void stop()
+    {
+      ticking = false;
+    }
+
+    template<class cb, class ...vargs>
+    void on_tick(cb callback, vargs... args)
+    {
+      while (true)
+      {
+        if (ticking)
+        {
+          callback(std::forward<vargs>(args)...);
+          this_thread::sleep_for(interval);
+        }
+      }
+    }
+  };
+}
